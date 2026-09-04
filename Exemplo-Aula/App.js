029d3531b049c54e29fec03b354125df0b0d19b5
aparecidalml <aparecidalml0@gmail.com>
2026-09-03 21:04:09 -0300

    ajustes

--- Exemplo-Aula/App.js
+++ Exemplo-Aula/App.js
@@ -10,20 +10,38 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons'
+// import { createDrawerNavigator } from '@react-navigation/drawer'
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
+// const Drawer = createDrawerNavigator()
+
+// const DrawerNavigator = ({route}) => {
+//   const {nome} = route.params
+//   return(
+//      <Drawer.Navigator >
+//       <Drawer.Screen name="Perfil" component={Perfil} 
+//         initialParams={{ nome: nome }} options={{ headerShown: false }}
+//       />
+//       <Drawer.Screen name="Configuracao" component={Configuracao} 
+//         options={{ headerShown: false }}
+//       />
+//     </Drawer.Navigator>
+//   )
+// }
 
 const TabNavigator = ({route}) => {
   const { nome } = route.params;
   return(
     <Tab.Navigator >
       <Tab.Screen name="Perfil" component={Perfil} 
-        initialParams={{ nome: nome }}
+        initialParams={{ nome: nome }} options={{ headerShown: false }}
         options={{tabBarIcon: ({color, size}) => (
           <Ionicons name='person' size={size} color={'green'} />
         )}}/>
-      <Tab.Screen name="Configuracao" component={Configuracao} initialParams = {route.params.nome} options={{tabBarIcon: ({color, size}) => (
+      <Tab.Screen name="Configuracao" component={Configuracao} 
+         options={{ headerShown: false }}
+        options={{tabBarIcon: ({color, size}) => (
           <Ionicons name='settings' size={size} color={'green'} />
         )}}/>
     </Tab.Navigator>
